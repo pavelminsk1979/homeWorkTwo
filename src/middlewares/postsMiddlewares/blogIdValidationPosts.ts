1ab9922e3f332dfa11c439fbb2d13c442ca5168b
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-02-14 03:35:24 +0300

    correct...

--- src/middlewares/postsMiddlewares/blogIdValidationPosts.ts
+++ src/middlewares/postsMiddlewares/blogIdValidationPosts.ts
@@ -1,4 +1,32 @@
 
+
+import {body} from "express-validator";
+import {DB} from "../../db/db";
+
+const findValue=(value:string)=>{
+   let blog= DB.blogs.find(e=>e.id===value)
+    return blog?.id
+}
+export const blogIdValidationPosts = body('blogId')
+    .exists()
+    .trim()
+    .custom((value) => value===findValue(value))
+    .withMessage('Incorrect blogId')
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import {body} from "express-validator";
 import {DB} from "../../db/db";
 
@@ -7,4 +35,4 @@ export const blogIdValidationPosts = body('blogId')
     .exists()
     .trim()
     .custom((value) => value===DB.blogs[0].id)
-    .withMessage('Incorrect blogId')
+    .withMessage('Incorrect blogId')*/
